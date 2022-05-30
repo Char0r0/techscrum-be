08a91a65161fa75c857079fb2e21dcf053202b81
Ting Li <tingl0606@gmail.com>
2022-05-30 22:11:36 +1000

    user page

--- src/app/routes/v1/api.ts
+++ src/app/routes/v1/api.ts
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = new express.Router();
 const weatherController = require("../../controllers/example");
-
+const projects = require("../../controllers/v1/projects/projectsController");
 router.get("/weathers", weatherController.index);
+
+router.get("/projects", projects.show);
+router.put("/projects", projects.update);
 
 module.exports = router;
