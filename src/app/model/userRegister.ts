07daec4c76dd7dc7026acf75ee551719d47fc9c0
Emil <350383656@qq.com>
2022-06-05 12:51:29 +1000

    connect db

--- src/app/model/userRegister.ts
+++ src/app/model/userRegister.ts
@@ -1,5 +1,16 @@
+import { Schema, model } from 'mongoose';
+
 export interface UserRigister {
     email: string,
     name: string,
     password: string,
 }
+
+const registerSchema = new Schema<UserRigister>({
+    email: { type: 'string', required: true},
+    name: { type: 'string', required: true},
+    password: { type: 'string', required: true}
+});
+
+const register = model<UserRigister>('userAccount', registerSchema);
+module.exports = register;
--- src/app/services/register/register.ts
+++ src/app/services/register/register.ts
@@ -1,26 +1,20 @@
 import { UserRigister } from "../../model/userRegister"
 import encryption from '../encryption/encryption'
 
-const users = Array<UserRigister>({
-    email: 'abc@gmail.com',
-    name: "Kuro",
-    password: '123456',
-},{
-    email: 'def@gmail.com',
-    name: "Kuro2",
-    password: '123456',
-});
+const registerSchema = require('../../model/userRegister');
 
-export const emailCheck = (email: string) => {
-    const index = users.findIndex(user => user.email === email);
-    if(index >= 0) return false;
+export const emailCheck = async (email: string) => {
+    const result = await registerSchema.find({ email });
+    if(result) return false;
     return true;
 }
 
 export const register = async (email: string, name: string, password: string) =>{
+    const userNotExistFlag = await emailCheck(email);
+    if(!userNotExistFlag) return false;
+
     const user: UserRigister = { email, name, password: await encryption(password) };
-    const index = users.findIndex(user => user.email === email);
-    if(index >= 0) return false;
-    users.push(user)
+    const newUser = new registerSchema(user);
+    await newUser.save();
     return true;
 }
