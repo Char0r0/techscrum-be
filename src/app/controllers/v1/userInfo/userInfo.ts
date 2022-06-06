65945a43433386ba35f3a9202ef105e629a7143d
Regan Leung <leungregan2021@gmail.com>
2022-06-06 22:11:28 +1000

    userInfo bug fixed

--- src/app/controllers/v1/userInfo/userInfo.ts
+++ src/app/controllers/v1/userInfo/userInfo.ts
@@ -3,12 +3,7 @@ import { users } from "../../../model/userDB";
 
 exports.index = (req: Request, res: Response) => {
   const user = req.user;
-  const searchUser = () => {
-    for (let i = 0; i < users.length; i++) {
-      if (users[i].email === user) return users[i].email;
-    }
-  };
-  const result = users.find(() => searchUser() === user);
+  const result = users.find((search) => search.email === user);
 
   res.json(result);
 };
