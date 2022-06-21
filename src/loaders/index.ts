289635ddfdc0841e9874a4937599956a3fdfc563
kitman20022002 <kitman200220022002@gmail.com>
2022-06-21 21:20:14 +1000

    complete remove swagger

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,9 +1,7 @@
 const expressLoader = require("./express");
-const swaggerLoader = require("./swagger");
 const mongooseLoader = require("./mongoose");
 
 exports.init = () => {
   const app = expressLoader();
-  //swaggerLoader(app);
   mongooseLoader();
 };
