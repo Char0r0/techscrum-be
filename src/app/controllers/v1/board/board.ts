59c93fedba68e125f32fc5ab6049b7123f9570ee
kitman20022002 <kitman200220022002@gmail.com>
2022-06-29 22:56:20 +1000

    fix crashing issue

--- src/app/controllers/v1/board/board.ts
+++ src/app/controllers/v1/board/board.ts
@@ -6,13 +6,12 @@ const board = require('../../../model/board');
 
 exports.show = async (req: Request, res: Response) => {
   const boardId = req.params.boardId;
-
   try {
     const boardInfo = await board.findBoardById(boardId);
     const boardinfoString = replaceAll(JSON.stringify(boardInfo), '_id', 'id');
     return res.send(JSON.parse(boardinfoString));
   } catch (e) {
-    if (e instanceof Error) return res.sendStatus(404).send({});
+    if (e instanceof Error) return res.sendStatus(404).send({ Error: e.message });
     return res.sendStatus(500).send({ Error: 'Unknow Error' });
   }
 };
