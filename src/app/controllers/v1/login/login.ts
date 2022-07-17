9b8c056c05a1e9cf83f5d4b1f222749679e12f9d
kitman <kitman200220022002@gmail.com>
2022-07-17 18:14:08 +1000

    fix login

--- src/app/controllers/v1/login/login.ts
+++ src/app/controllers/v1/login/login.ts
@@ -6,7 +6,7 @@ exports.store = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const user = await User.getModel(req.dbConnection).findByCredentials(req.body.email, req.body.password);
     const userProfile = await UserProfile.getModel(req.dbConnection).findNameAndIconById(user._id);
-    const token = await user.getModel(req.dbConnection).generateAuthToken();
+    const token = await user.generateAuthToken();
     res.send({ user, userProfile, ...token });
   } catch (e) {
     next(e);
