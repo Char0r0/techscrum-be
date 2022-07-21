4bf7f64a9628d79fd168f5464e264c84a71fd175
Emil <350383656@qq.com>
2022-07-21 14:18:19 +1000

    TEC-194 fixed login response problem

--- src/app/controllers/v1/login/login.ts
+++ src/app/controllers/v1/login/login.ts
@@ -1,10 +1,15 @@
 import { Response, Request, NextFunction } from 'express';
 const User = require('../../../model/user');
 const UserProfile = require('../../../model/userProfile');
+const status = require('http-status');
 
-exports.store = async (req: Request, res: Response, next: NextFunction) => {
+exports.login = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const user = await User.getModel(req.dbConnection).findByCredentials(req.body.email, req.body.password);
+    const user = await User.getModel(req.dbConnection).findByCredentials(
+      req.body.email,
+      req.body.password,
+    );
+    if (user == null || user == undefined) return res.status(status.UNAUTHORIZED).send();
     const userProfile = await UserProfile.getModel(req.dbConnection).findNameAndIconById(user._id);
     const token = await user.generateAuthToken();
     res.send({ user, userProfile, ...token });
