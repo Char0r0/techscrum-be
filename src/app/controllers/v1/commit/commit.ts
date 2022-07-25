34031dc00e7d9fd0ad3e5f59931678a07dc7f94d
Hyna <shirleymm118@gmail.com>
2022-07-25 21:10:55 +1000

    TEC-178 delete useless codes

--- src/app/controllers/v1/commit/commit.ts
+++ src/app/controllers/v1/commit/commit.ts
@@ -1,17 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 const commits = require('../../../model/commit');
-const user = require('../../../model/user');
 const status = require('http-status');
 const { replaceId } = require('../../../services/replace/replace');
 
-exports.show = async (req: Request, res: Response, next: NextFunction) => {
-  // const { id } = req.params;
-  // try {
-  //   const result = await commits.getModel(req.dbConnection).find({ senderId: id });
-  //   res.send(replaceId(result));
-  // } catch (e) {
-  //   next(e);
-  // }
+exports.show = async (req: Request, res: Response) => {
   res.send([]);
 };
 
