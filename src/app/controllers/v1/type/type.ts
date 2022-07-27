769876a9d5348894d723097e7067c568ac9deaa0
kitman <kitman200220022002@gmail.com>
2022-07-27 18:08:47 +1000

    added type api

---
+++ src/app/controllers/v1/type/type.ts
@@ -0,0 +1,11 @@
+export {};
+import { Request, Response } from 'express';
+import { replaceId } from '../../../services/replace/replace';
+
+const Type = require('../../../model/type');
+
+exports.index = async (req: Request, res: Response) => {
+  const typeModel = Type.getModel(req.dbConnection);
+  const result = await typeModel.find();
+  res.send(replaceId(result));
+};
--- src/app/database/init.ts
+++ src/app/database/init.ts
@@ -1,12 +1,20 @@
 export {};
 const Role = require('../model/role');
 const Permission = require('../model/permission');
-
+const Type = require('../model/type');
 exports.init = async (dbConnection:any) =>{
   
   const role = Role.getModel(dbConnection);
   const permission = Permission.getModel(dbConnection);
+  const type = Type.getModel(dbConnection);
     
+  const type1 = new type({ name:'Story', slug:'story' });
+  const type2 = new type({ name:'Task', slug:'task' });
+  const type3 = new type({ name:'Bug', slug:'bug' });
+  await type1.save();
+  await type2.save();
+  await type3.save();
+
   const result = await role.find({ slug:'admin' });
   if (result.length > 0) {
     return;
@@ -26,66 +34,84 @@ exports.init = async (dbConnection:any) =>{
   const editProjectPolicy = new permission({ slug: 'edit:projects', description: 'Edit Project' });
   const deleteProjectPolicy = new permission({ slug: 'delete:projects', description: 'Delete Project' });
 
+  const createBoardPolicy = new permission({ slug: 'create:boards', description: 'Create Boards' });
   const viewBoardPolicy = new permission({ slug: 'view:boards', description: 'View Boards' });
   const editBoardPolicy = new permission({ slug: 'edit:boards', description: 'Edit Project' });
   const deleteBoardPolicy = new permission({ slug: 'delete:boards', description: 'Delete Project' });
 
-  const addMemberPolicy = new permission({ slug: 'add:members', description: 'Add Members' });
-  const viewMemberPolicy = new permission({ slug: 'view:members', description: 'View Members' });
-  const editMemberPolicy = new permission({ slug: 'edit:members', description: 'Edit Members' });
-  const deleteMemberPolicy = new permission({ slug: 'delete:members', description: 'Delete Members' });
+  const addMembersPolicy = new permission({ slug: 'add:members', description: 'Add Members' });
+  const viewMembersPolicy = new permission({ slug: 'view:members', description: 'View Members' });
+  const editMembersPolicy = new permission({ slug: 'edit:members', description: 'Edit Members' });
+  const deleteMembersPolicy = new permission({ slug: 'delete:members', description: 'Delete Members' });
 
   const addRolesPolicy = new permission({ slug: 'add:roles', description: 'Add Roles' });
   const viewRolesPolicy = new permission({ slug: 'view:roles', description: 'View Roles' });
   const editRolesPolicy = new permission({ slug: 'edit:roles', description: 'Edit Roles' });
   const deleteRolesPolicy = new permission({ slug: 'delete:roles', description: 'Delete Roles' });
 
-  const addShortcutPolicy = new permission({ slug: 'add:shortcut', description: 'Add Shortcut' });
-  const viewShortcutPolicy = new permission({ slug: 'view:shortcut', description: 'View Shortcut' });
-  const editShortcutPolicy = new permission({ slug: 'edit:shortcut', description: 'Edit Shortcut' });
-  const deleteShortcutPolicy = new permission({ slug: 'delete:shortcut', description: 'Delete Shortcut' });
+  const addShortcutsPolicy = new permission({ slug: 'add:shortcuts', description: 'Add Shortcuts' });
+  const viewShortcutsPolicy = new permission({ slug: 'view:shortcuts', description: 'View Shortcuts' });
+  const editShortcutsPolicy = new permission({ slug: 'edit:shortcuts', description: 'Edit Shortcuts' });
+  const deleteShortcutsPolicy = new permission({ slug: 'delete:shortcuts', description: 'Delete Shortcuts' });
+
+  const addCardsPolicy = new permission({ slug: 'add:tasks', description: 'Add Tasks' });
+  const viewCardsPolicy = new permission({ slug: 'view:tasks', description: 'View Tasks' });
+  const editCardsPolicy = new permission({ slug: 'edit:tasks', description: 'Edit Tasks' });
+  const deleteCardsPolicy = new permission({ slug: 'delete:tasks', description: 'Delete Tasks' });
+
+  const viewSettingsPolicy = new permission({ slug: 'view:settings', description: 'View Settings' });
+  const editSettingsPolicy = new permission({ slug: 'edit:settings', description: 'Edit Settings' });
 
   await viewProjectPolicy.save();
   await editProjectPolicy.save();
   await deleteProjectPolicy.save();
+
+  await createBoardPolicy.save();
   await viewBoardPolicy.save();
   await editBoardPolicy.save();
   await deleteBoardPolicy.save();
 
-  await addMemberPolicy.save();
-  await viewMemberPolicy.save();
-  await editMemberPolicy.save();
-  await deleteMemberPolicy.save();
+  await addMembersPolicy.save();
+  await viewMembersPolicy.save();
+  await editMembersPolicy.save();
+  await deleteMembersPolicy.save();
 
   await addRolesPolicy.save();
   await viewRolesPolicy.save();
   await editRolesPolicy.save();
   await deleteRolesPolicy.save();
 
-  await addShortcutPolicy.save();
-  await viewShortcutPolicy.save();
-  await editShortcutPolicy.save();
-  await deleteShortcutPolicy.save();
+  await addShortcutsPolicy.save();
+  await viewShortcutsPolicy.save();
+  await editShortcutsPolicy.save();
+  await deleteShortcutsPolicy.save();
   ////////////////////////////////
   const permissions = [
     viewProjectPolicy._id, 
     editProjectPolicy._id, 
     deleteProjectPolicy._id, 
+    createBoardPolicy._id,
     viewBoardPolicy._id, 
     editBoardPolicy._id, 
     deleteBoardPolicy._id, 
-    addMemberPolicy._id, 
-    viewMemberPolicy._id,
-    editMemberPolicy._id, 
-    deleteMemberPolicy._id, 
+    addMembersPolicy._id, 
+    viewMembersPolicy._id,
+    editMembersPolicy._id, 
+    deleteMembersPolicy._id, 
     addRolesPolicy._id, 
     viewRolesPolicy._id, 
     editRolesPolicy._id, 
     deleteRolesPolicy._id, 
-    addShortcutPolicy._id, 
-    viewShortcutPolicy._id,
-    editShortcutPolicy._id,
-    deleteShortcutPolicy._id,
+    addShortcutsPolicy._id, 
+    viewShortcutsPolicy._id,
+    editShortcutsPolicy._id,
+    deleteShortcutsPolicy._id,
+    addCardsPolicy._id,
+    viewCardsPolicy._id,
+    editCardsPolicy._id,
+    deleteCardsPolicy.id,
+    viewSettingsPolicy._id,
+    editSettingsPolicy._id,
   ];
 
 
@@ -96,17 +122,17 @@ exports.init = async (dbConnection:any) =>{
     viewBoardPolicy._id, 
     editBoardPolicy._id, 
     deleteBoardPolicy._id, 
-    addShortcutPolicy._id, 
-    viewShortcutPolicy._id,
-    editShortcutPolicy._id,
-    deleteShortcutPolicy._id,
+    addShortcutsPolicy._id, 
+    viewShortcutsPolicy._id,
+    editShortcutsPolicy._id,
+    deleteShortcutsPolicy._id,
   ];
 
 
   const guestPermissions = [
     viewProjectPolicy._id, 
     viewBoardPolicy._id,  
-    viewShortcutPolicy._id,
+    viewShortcutsPolicy._id,
   ];
 
   adminRole.permission = permissions;
