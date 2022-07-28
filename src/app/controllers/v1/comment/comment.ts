b8db90d9ad8f83ab10c77dd5b6ffb69c3ca4ca30
kitman <kitman200220022002@gmail.com>
2022-07-28 18:39:36 +1000

    revert commit

--- src/app/controllers/v1/comment/comment.ts
+++ src/app/controllers/v1/comment/comment.ts
@@ -1,47 +1,49 @@
 import { Request, Response, NextFunction } from 'express';
-const Comment = require('../../../model/comment');
-const Task = require('../../../model/task');
-const mongoose = require('mongoose');
+const comment = require('../../../model/comment');
+const user = require('../../../model/user');
 const status = require('http-status');
 const { replaceId } = require('../../../services/replace/replace');
-import { validationResult } from 'express-validator';
+
+exports.show = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  try {
+    const result = await comment
+      .getModel(req.dbConnection)
+      .find({})
+      .populate({ path: 'senderId', user });
+    res.send(replaceId(result));
+  } catch (e) {
+    next(e);
+  }
+  //res.send([]);
+};
 
 exports.store = async (req: Request, res: Response, next: NextFunction) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.sendStatus(status.UNPROCESSABLE_ENTITY);
-  }
+  const { taskId, senderId, content } = req.body;
   try {
-    const commentModel = Comment.getModel(req.dbConnection);
-
-    let result = new commentModel({
-      senderId: req.body.senderId,
-      content: req.body.content,
+    const newComment = await comment.getModel(req.dbConnection).create({
+      taskId,
+      senderId,
+      content,
     });
-    result.save();
-    const taskModel = Task.getModel(req.dbConnection);
-    const task = await taskModel.findById(req.params.taskId);
-    task.comments.push(mongoose.Types.ObjectId(result._id));
-    task.save();
-    return res.send(replaceId(result));
+    if (!newComment) {
+      res.sendStatus(status.UNPROCESSABLE_ENTITY);
+      return;
+    }
+    res.send(replaceId(newComment));
   } catch (e) {
     next(e);
   }
 };
 
 exports.update = async (req: Request, res: Response, next: NextFunction) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(status.UNPROCESSABLE_ENTITY).json({});
-  }
+  const { id } = req.params;
   const { content } = req.body;
   const updatedAt = Date.now();
   try {
-    const updatedComment = await Comment.getModel(req.dbConnection).findByIdAndUpdate(
-      req.params.id,
-      { content, updatedAt },
-      { new: true },
-    );
+    const updatedComment = await comment
+      .getModel(req.dbConnection)
+      .findByIdAndUpdate({ _id: id }, { content, updatedAt }, { new: true });
     if (!updatedComment) {
       res.sendStatus(status.UNPROCESSABLE_ENTITY);
       return;
@@ -53,33 +55,11 @@ exports.update = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 exports.destroy = async (req: Request, res: Response, next: NextFunction) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(status.UNPROCESSABLE_ENTITY).json({});
-  }
-  const { taskId, commentId } = req.params;
+  const { id } = req.params;
   try {
-    const checkCommentExistUnderComment = await Comment.getModel(req.dbConnection).findById({
-      _id: commentId,
-    });
-    const checkCommentExistUnderTask = await Task.getModel(req.dbConnection).find({
-      'comments._id': commentId,
-    });
-    if (!checkCommentExistUnderComment && checkCommentExistUnderTask.length === 0) {
+    const deleteComment = await comment.getModel(req.dbConnection).findByIdAndDelete({ _id: id });
+    if (!deleteComment) {
       res.sendStatus(status.NOT_FOUND);
-      return;
-    }
-    const taskModel = Task.getModel(req.dbConnection);
-    const task = await taskModel.findById(taskId);
-    task.comments = await task.comments.filter((item: any) => {
-      return item._id.toString() !== commentId;
-    });
-    const removeCommentUnderTask = await task.save();
-    const deleteComment = await Comment.getModel(req.dbConnection).findByIdAndRemove({
-      _id: commentId,
-    });
-    if (!removeCommentUnderTask && !deleteComment) {
-      res.sendStatus(status.BAD_REQUEST);
       return;
     }
     res.sendStatus(status.NO_CONTENT);
