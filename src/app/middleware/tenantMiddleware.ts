b787427e21d846a24c2d9c4d5d6766cff0792a61
kitman20022002 <kitman200220022002@gmail.com>
2022-09-11 16:16:12 +1000

    Added saas default connection

--- src/app/middleware/tenantMiddleware.ts
+++ src/app/middleware/tenantMiddleware.ts
@@ -5,13 +5,14 @@ const connection = require('../utils/db');
 const config = require('../config/app');
 exports.dbContextInitializer = (req: Request, res: Response, next: NextFunction) => {
   let origin = req.headers.origin;
+  let connectDB = config.tenantConnection;
   if (!origin) {
     res.status(403).send({
       'response': 'Invalid database request',
     });
   }
 
-  let Tenants = connection(config.tenantConnection).model('tenants', TenantSchema);
+  let Tenants = connection(connectDB).model('tenants', TenantSchema);
 
   Tenants.findOne({
     origin: origin,
