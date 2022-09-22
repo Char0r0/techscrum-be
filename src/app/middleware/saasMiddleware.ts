37b653ae99476a0351d4142697363de8d4bba1cf
kitman20022002 <kitman200220022002@gmail.com>
2022-09-22 21:48:46 +1000

    fix sonar

--- src/app/middleware/saasMiddleware.ts
+++ src/app/middleware/saasMiddleware.ts
@@ -5,9 +5,11 @@ const Tenant = require('../model/tenant');
 const config = require('../../app/config/app');
 const { dataConnectionPool, tenantConnection } = require('../utils/dbContext');
 const logger = require('../../loaders/logger');
-const saas = async (req: Request, res: Response, next: NextFunction) => {
-  let tenantId: string = config.defaultTenantConnection || 'devtechscrumapp';
-  const domain  = req.headers.origin;
+
+const getTenantId = async (domain:string | undefined) => {
+  if (!domain) {
+    return config.defaultTenantConnection || 'devtechscrumapp';
+  }
   const excludeDomain = domain === 'https://www.techscrumapp.com' || domain === 'http://localhost:3000';
 
   if (config.useDefaultDatabase.toString() === false.toString()) {
@@ -21,16 +23,23 @@ const saas = async (req: Request, res: Response, next: NextFunction) => {
       const result = await tenantModel.findOne({ origin: domain } );
       if (!config || !config.emailSecret) {
         logger.error('Missing email secret in env');
-        return null;
+        throw new Error('Missing email secret in env');
       }
       if (!result) {
         logger.error('Cannot find tanant result');   
-        return null;
+        throw new Error('Cannot find tanant result');
       }
-      tenantId = result._id?.toString();
+      return result._id?.toString();
     }
   }
+  return config.defaultTenantConnection || 'devtechscrumapp';
+};
+
+const saas = async (req: Request, res: Response, next: NextFunction) => {
+  const domain  = req.headers.origin;
+  const tenantId: string = await getTenantId(domain);
   const url = config.db.replace('techscrumapp', tenantId);
+  
   if (!dataConnectionPool || !dataConnectionPool[tenantId]) {
     const dataConnectionMongoose = new Mongoose();
     dataConnectionMongoose.connect(url).then(() => {
