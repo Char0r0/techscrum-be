41627fbf775e8b9b9e3ec835977367bdda63011a
kitman20022002 <kitman200220022002@gmail.com>
2022-09-29 18:54:50 +1000

    added domain api

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -5,6 +5,7 @@ dotenv.config();
 const DEFAULT_MONGODB_URL = 'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/techscrumapp?retryWrites=true&w=majority';
 const DEFAULT_FE_URL = 'http://localhost:3000';
 const DEFAULT_TANANT_CONNECTION = 'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/tenant?retryWrites=true&w=majority';
+const DEFAULT_DOMAIN_CONNECTION = 'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/domain?retryWrites=true&w=majority';
 module.exports = {
   name: process.env.NAME || 'techscrumapp',
   port: process.env.PORT || 8000,
@@ -16,6 +17,7 @@ module.exports = {
     process.env.MONGODB_URL || DEFAULT_MONGODB_URL,
   frontEndAddress: process.env.FRONTEND_ADDRESS || DEFAULT_FE_URL,
   tenantConnection: DEFAULT_TANANT_CONNECTION,
+  domainConnection: DEFAULT_DOMAIN_CONNECTION,
   useDefaultDatabase: process.env.USE_DEFAULT_DATABASE || true,
   companyAddress: process.env.COMPANY_ADDRESS || 'su93031093@gmail.com',
   defaultTenantConnection: process.env.DEFAULT_TENANT_CONNECTION || 'devtechscrumapp',
--- src/app/utils/helper.ts
+++ src/app/utils/helper.ts
@@ -1,5 +1,31 @@
 import { NextFunction } from 'express';
-
+import { Mongoose } from 'mongoose';
+const { domainConnection } = require('../utils/dbContext');
+const Domain = require('../model/domain');
+const config = require('../../app/config/app');
 export const asyncHandler = (fn:any) => (req: Request, res: Response, next: NextFunction)=>{
   return Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+export const getDomains = async () => {
+  const haveDomainConnection = Object.keys(domainConnection).length !== 0;
+  if (!haveDomainConnection) {
+    const dataConnectionMongoose = new Mongoose();
+    domainConnection.connection  = await dataConnectionMongoose.connect(config.domainConnection);
+  }
+  return Domain.getModel(domainConnection.connection).find({});
+};
+
+export const shouldExcludeDomainList = async (host: string | undefined) => {
+  if (!host) {
+    return false;
+  }
+  const domains: any  = await getDomains();
+
+  for (const domain in domains) {
+    if (host.includes(domains[domain].url)) {
+      return true;
+    }
+  }
+  return false;
 };
