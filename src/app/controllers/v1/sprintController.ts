04856b96a7efa402df6185f656f6d2b0e494edae
Joe Zhou <zhouhaohangau@gmail.com>
2022-10-04 17:40:29 +1100

    added index, show, update and destroy controller to sprint

--- src/app/controllers/v1/sprintController.ts
+++ src/app/controllers/v1/sprintController.ts
@@ -1,13 +1,67 @@
 import { Request, Response, NextFunction } from 'express';
+import { asyncHandler } from '../../utils/helper';
 const status = require('http-status');
 const Sprint = require('../../model/sprint');
-exports.store = async (req: Request, res: Response, next: NextFunction) => {
+const Project = require('../../model/project');
+const Board = require('../../model/board');
+const Task = require('../../model/task');
+
+// get all
+export const index = asyncHandler(async (req: Request, res: Response) => {
+  const sprintModel = Sprint.getModel(req.dbConnection);
+  const sprints = await sprintModel
+    .find({})
+    .populate({ path: 'projectId', model: Project.getModel(req.dbConnection) })
+    .populate({ path: 'boardId', model: Board.getModel(req.dbConnection) })
+    .populate({ path: 'taskId', model: Task.getModel(req.dbConnection) });
+
+  res.status(status.OK).json(sprints);
+});
+
+// get one
+export const show = asyncHandler(async (req: Request, res: Response) => {
+  const sprintModel = Sprint.getModel(req.dbConnection);
+  const { id } = req.params;
+  const sprint = await sprintModel
+    .findById(id)
+    .populate({ path: 'projectId', model: Project.getModel(req.dbConnection) })
+    .populate({ path: 'boardId', model: Board.getModel(req.dbConnection) })
+    .populate({ path: 'taskId', model: Task.getModel(req.dbConnection) });
+
+  res.status(status.OK).json(sprint);
+});
+
+// create
+export const store = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const sprintModel = Sprint.getModel(req.dbConnection);
-    const sprint = new sprintModel({ name: req.body.name, description:req.body.desc, isComplete:false });
+    const sprint = new sprintModel(req.body);
     await sprint.save();
     res.status(status.CREATED).send(sprint);
   } catch (e) {
     next(e);
   }
 };
+
+// update
+export const update = asyncHandler(async (req: Request, res: Response) => {
+  const sprintModel = Sprint.getModel(req.dbConnection);
+  const { id } = req.params;
+  const updatedSprint = await sprintModel
+    .findByIdAndUpdate(id, req.body, {
+      returnDocument: 'after',
+    })
+    .populate({ path: 'projectId', model: Project.getModel(req.dbConnection) })
+    .populate({ path: 'boardId', model: Board.getModel(req.dbConnection) })
+    .populate({ path: 'taskId', model: Task.getModel(req.dbConnection) });
+
+  res.status(status.OK).json(updatedSprint);
+});
+
+// delete
+export const destroy = asyncHandler(async (req: Request, res: Response) => {
+  const sprintModel = Sprint.getModel(req.dbConnection);
+  const { id } = req.query;
+  const deletedSprint = await sprintModel.findOneAndDelete(id);
+  res.status(status.OK).json(deletedSprint);
+});
