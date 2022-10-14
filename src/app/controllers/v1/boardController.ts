64cf091fe4a9b628e48c0e1d4c0062034513f456
Joe Zhou <zhouhaohangau@gmail.com>
2022-10-15 02:31:35 +1100

    updated GET/:id in board controller

--- src/app/controllers/v1/boardController.ts
+++ src/app/controllers/v1/boardController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
 const status = require('http-status');
-const Board = require('../../model/board');
 const { replaceId } = require('../../services/replaceService');
 import { validationResult } from 'express-validator';
-
+import * as Board from '../../model/board';
+import * as Status from '../../model/status';
+// GET one
 exports.show = async (req: Request, res: Response) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -16,6 +17,9 @@ exports.show = async (req: Request, res: Response) => {
     return;
   }
 
-  const boardInfo = await Board.getModel(req.dbConnection).findBoardById(boardId);
+  const boardInfo = await Board.getModel(req.dbConnection)
+    .findById(boardId)
+    .populate({ path: 'taskStatus', model: Status.getModel(req.dbConnection) });
+
   res.send(replaceId(boardInfo));
 };
