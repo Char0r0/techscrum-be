1725dec6426f791c43ed66f42053c644e81de550
Joe Zhou <zhouhaohangau@gmail.com>
2022-10-15 02:21:46 +1100

    added status model

---
+++ src/app/model/status.ts
@@ -0,0 +1,35 @@
+import { Mongoose, Schema } from 'mongoose';
+
+export interface IStatus {
+  name: string;
+  order: number;
+  tasks: Schema.Types.ObjectId[];
+}
+
+const statusSchema = new Schema<IStatus>(
+  {
+    name: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    order: {
+      type: Schema.Types.Number,
+      default: 0,
+    },
+    tasks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'tasks',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const getModel = (dbConnection: Mongoose) => {
+  if (!dbConnection) throw new Error('No connection');
+
+  return dbConnection.model('statuses', statusSchema);
+};
