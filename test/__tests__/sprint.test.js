9ab10b4ee07fd37fa8b9be183970bbb238544b09
Joe Zhou <zhouhaohangau@gmail.com>
2022-11-11 23:02:38 +1100

    updated sprint test

--- test/__tests__/sprint.test.js
+++ test/__tests__/sprint.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const { SPRINT_SEED } = require('../fixtures/sprint');
 const { setup, restore } = require('../helpers');
 
 let application = null;
@@ -39,7 +40,7 @@ describe('POST sprint', () => {
     });
   });
 
-  it.only('should create a sprint with extra info', async () => {
+  it('should create a sprint with extra info', async () => {
     const endDate = new Date(2022, 12, 1);
 
     const res = await request(application)
@@ -72,8 +73,6 @@ describe('POST sprint', () => {
     ${'name'}       | ${undefined}
     ${'boardId'}    | ${undefined}
     ${'projectId'}  | ${undefined}
-    ${'startDate'}  | ${'not a date'}
-    ${'endDate'}    | ${'not a date'}
     ${'isComplete'} | ${'not a boolean'}
   `('shoudl return 422 if $field is $value is provided', async ({ field, value }) => {
     const res = await request(application)
@@ -83,5 +82,65 @@ describe('POST sprint', () => {
         [field]: value,
       });
     expect(res.statusCode).toBe(422);
+  });
+});
+
+describe('UPDATE sprint', () => {
+  it('should update a sprint if valid info is provided', async () => {
+    const res = await request(application)
+      .put(`${baseURL}/${SPRINT_SEED._id}`)
+      .send({
+        ...SPRINT_SEED,
+        name: 'updated name',
+        description: 'updated description',
+      });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({
+      __v: 0,
+      _id: '63463fb9788a44fa544b4a9a',
+      boardId: {
+        _id: '6350d443bddbe8fed0138ffd',
+        createdAt: expect.any(String),
+        taskStatus: [
+          '6350d443bddbe8fed0138ff4',
+          '6350d443bddbe8fed0138ff5',
+          '6350d443bddbe8fed0138ff6',
+          '6350d443bddbe8fed0138ff7',
+        ],
+        title: 'test board',
+        updatedAt: expect.any(String),
+      },
+      createdAt: expect.any(String),
+      description: 'updated description',
+      endDate: null,
+      isComplete: false,
+      name: 'updated name',
+      projectId: null,
+      startDate: expect.any(String),
+      taskId: [],
+      updatedAt: expect.any(String),
+    });
+  });
+
+  it('should return 404 if no resource found', async () => {
+    const res = await request(application).put(`${baseURL}/6350d443bddbe8fed0138ff4`).send({
+      name: 'updated name',
+    });
+
+    expect(res.statusCode).toBe(404);
+  });
+});
+
+describe('DELETE sprint', () => {
+  it('should delete a sprint with correct id', async () => {
+    const res = await request(application).delete(`${baseURL}/${SPRINT_SEED._id}`);
+    expect(res.statusCode).toBe(204);
+    expect(res.body).toEqual({});
+  });
+
+  it('should return 404 with incorrect id', async () => {
+    const res = await request(application).delete(`${baseURL}/6350d443b82be8fed0138ff2`);
+    expect(res.statusCode).toBe(404);
   });
 });
