58c35a90c1c1ecd7bb6c60b59e79d7867e78358a
kitman <kitman200220022002@gmail.com>
2022-11-19 17:55:30 +1100

    added swagger and docker

--- src/loaders/swagger.ts
+++ src/loaders/swagger.ts
@@ -1,38 +1,8 @@
 const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
-
-const config = require('../app/config/app');
-const swaggerConfig = require('../app/config/swagger');
-
+import * as data from '../../swagger_output.json';
 import { Express } from 'express';
 
-const options = {
-  definition: {
-    openapi: swaggerConfig.openapi,
-    info: {
-      title: config.name,
-      version: config.version,
-    },
-    servers: [
-      {
-        url: 'http://localhost:{port}{basePath}',
-        variables: {
-          port: {
-            default: config.port,
-          },
-          basePath: {
-            default: config.api.prefix,
-          },
-        },
-        description: 'Local Server',
-      },
-    ],
-  },
-  apis: ['./src/app/routes/v1/api.ts'],
-};
-
 module.exports = (app: Express) => {
-  const swaggerSpec = swaggerJSDoc(options);
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(data));
   return app;
 };
