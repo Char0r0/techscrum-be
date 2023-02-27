36d153babbe2620e750bc61acbbc1a892b1b3d7f
ZhouTian0716 <egglord0716@gmail.com>
2023-02-27 20:52:12 +1100

    update for sonar

--- src/app/services/emailUsService.ts
+++ src/app/services/emailUsService.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 const { emailRecipientTemplate } = require('../utils/emailSender');
 
 const FULLNAME_REGEX = /^[a-z ,.'-]+$/i;
-const PHONE_REGEX = /^[0-9]{10}$/;
+const PHONE_REGEX = /^\d{10}$/;
 const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 const enquiryTitles = [
   'Just saying hi!',
