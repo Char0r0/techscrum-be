3912f3453a7bd2415413a7c43ffdef54f5cf0f12
Summy <s4594789@student.uq.edu.au>
2023-03-10 09:38:07 +1000

    registerv2 modification

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -2,9 +2,14 @@ const dotenv = require('dotenv');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 dotenv.config();
 
-const DEFAULT_MONGODB_URL = 'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/techscrumapp?retryWrites=true&w=majority';
-const DEFAULT_TANANT_CONNECTION = 'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/tenant?retryWrites=true&w=majority';
-const DEFAULT_DOMAIN_CONNECTION = 'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/domain?retryWrites=true&w=majority';
+const DEFAULT_MONGODB_URL =
+  'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/techscrumapp?retryWrites=true&w=majority';
+const DEFAULT_TANANT_CONNECTION =
+  'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/tenant?retryWrites=true&w=majority';
+const DEFAULT_TANANT_CONNECTION_V2 =
+  'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/tenantV2?retryWrites=true&w=majority';
+const DEFAULT_DOMAIN_CONNECTION =
+  'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/domain?retryWrites=true&w=majority';
 module.exports = {
   name: process.env.NAME || 'techscrumapp',
   port: process.env.PORT || 8000,
@@ -14,6 +19,7 @@ module.exports = {
   version: '1.0.0',
   db: process.env.MONGODB_URL || DEFAULT_MONGODB_URL,
   tenantConnection: process.env.TENANT_MONGODB_URL || DEFAULT_TANANT_CONNECTION,
+  tenantConnectionV2: process.env.TENANT_MONGODB_URL_V2 || DEFAULT_TANANT_CONNECTION_V2,
   domainConnection: process.env.DOMAIN_MONGODB_URL || DEFAULT_DOMAIN_CONNECTION,
   useDefaultDatabase: process.env.USE_DEFAULT_DATABASE || true,
   companyAddress: process.env.COMPANY_ADDRESS || 'su93031093@gmail.com',
--- src/app/controllers/v1/registerV2Controller.ts
+++ src/app/controllers/v1/registerV2Controller.ts
@@ -2,6 +2,10 @@ import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import { asyncHandler } from '../../utils/helper';
 import { Mongoose } from 'mongoose';
+import { randomStringGenerator } from '../../utils/randomStringGenerator';
+import { emailSender } from '../../utils/emailSender';
+const logger = require('../../../loaders/logger');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const status = require('http-status');
 const config = require('../../config/app');
@@ -16,33 +20,52 @@ export const register = asyncHandler(async (req: Request, res: Response) => {
   }
   const { email, company } = req.body;
 
+  let tenantsField;
+
+  const tenantsDbConnection = new Mongoose();
+  const resTenantsDbConnection = await tenantsDbConnection.connect(config.tenantConnectionV2);
+  const tenantModel = await Tenant.getModel(resTenantsDbConnection);
+  let tenantsUrl = `https://${company}.techscrumapp.com`;
+  if (company === 'localhost') {
+    tenantsUrl = 'http://localhost:3000';
+  }
   try {
-    const tenantsDbConnection = new Mongoose();
-    const resTenantsDbConnection = await tenantsDbConnection.connect(config.tenantConnection);
-    const tenantModel = await Tenant.getModel(resTenantsDbConnection);
-    const tenantsUrl = `https://${company}.techscrumapp.com`;
     const newTenants = await tenantModel.create({ origin: tenantsUrl });
-    const tenantsField = [mongoose.Types.ObjectId(newTenants.id)];
-
-    const userDbConnection = new Mongoose();
-    const userDbUrl = config.db.replace('techscrumapp', 'users');
-    const resUserDbConnection = await userDbConnection.connect(userDbUrl);
-
-    const userModel = User.getModel(resUserDbConnection);
-
-    let newUser;
-    newUser = await userModel.create({
-      email,
-      name: company,
-      active: false,
-      refreshToken: '',
-      activeCode: '',
-      tenants: tenantsField,
-    });
-
-    return res.status(200).json({ status: 'success', data: { newTenants, newUser } });
+    tenantsField = mongoose.Types.ObjectId(newTenants.id);
   } catch (e) {
-    // eslint-disable-next-line no-console
-    console.log(e);
+    return res.status(500).json({ status: 'The tenant has already exist' });
+  }
+
+  const userDbConnection = new Mongoose();
+  const userDbUrl = config.db.replace('techscrumapp', 'users');
+  const resUserDbConnection = await userDbConnection.connect(userDbUrl);
+  const userModel = User.getModel(resUserDbConnection);
+  const user = await userModel.findOne({ email });
+
+  try {
+    let newUser;
+    if (!user) {
+      const activeCode = randomStringGenerator(16);
+      newUser = await userModel.create({
+        email,
+        activeCode,
+        tenants: [tenantsField],
+      });
+      if (!config || !config.emailSecret) {
+        logger.error('Missing email secret in env');
+        return null;
+      }
+      const validationToken = jwt.sign({ email, activeCode }, config.emailSecret);
+      emailSender(email, `token=${validationToken}`, tenantsUrl);
+    } else {
+      user.tenants.push(tenantsField);
+      newUser = await user.save();
+    }
+    return res.status(200).json({ status: 'success', data: { newUser } });
+  } catch (e) {
+    if (user && user.tenants.length === 0) {
+      await userModel.deleteOne({ email });
+    }
+    return res.send(status.UNPROCESSABLE_ENTITY).json({ status: 'register failed' });
   }
 });
