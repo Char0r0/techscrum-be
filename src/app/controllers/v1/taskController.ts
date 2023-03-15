03789b91e8fa6464d1de4d7e574e0db2e0643f1d
ZhouTian0716 <egglord0716@gmail.com>
2023-03-15 23:15:37 +1100

    taskByProject route created

--- src/app/controllers/v1/taskController.ts
+++ src/app/controllers/v1/taskController.ts
@@ -6,6 +6,7 @@ const Task = require('../../model/task');
 const mongoose = require('mongoose');
 const Status = require('../../model/status');
 const Sprint = require('../../model/sprint');
+const Project = require('../../model/project');
 const httpStatus = require('http-status');
 const { validationResult } = require('express-validator');
 
@@ -23,6 +24,25 @@ exports.show = asyncHandler(async (req: Request, res: Response) => {
   }
   const tasks = await findTasks({ _id: req.params.id }, {}, {}, req.dbConnection);
   res.status(200).send(replaceId(tasks[0]));
+});
+
+// GET TASKS BY PROJECT
+exports.tasksByProject = asyncHandler(async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.sendStatus(httpStatus.UNPROCESSABLE_ENTITY).json({ errors: errors });
+  }
+  const { id: projectId } = req.params;
+  const tasks = await Task.getModel(req.dbConnection)
+    .find({ projectId: projectId })
+    .populate({
+      path: 'projectId',
+      model: Project.getModel(req.dbConnection),
+      select: 'key',
+    })
+    .sort({ createdAt: 1 })
+    .exec();
+  res.status(200).send(replaceId(tasks));
 });
 
 //POST
