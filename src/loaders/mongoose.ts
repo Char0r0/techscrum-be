ec086e126692237f62779ca5a0acd46d72be0780
kitman20022002 <kitman200220022002@gmail.com>
2023-03-30 15:22:20 +1100

    fix connection

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -1,18 +1,19 @@
 export {};
-const mongoose = require('mongoose');
-const config = require('../app/config/app');
-const logger = require('../loaders/logger');
+// const mongoose = require('mongoose');
+// const config = require('../app/config/app');
+// const logger = require('../loaders/logger');
 
 module.exports = async function () {
-  const connection = await mongoose
-    .connect(config.db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .catch((e: any) => {
-      logger.error('mongodb error', e);
-      process.exit(1);
-    });
+  // const connection = await mongoose
+  //   .connect(config.db, {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //   })
+  //   .catch((e: any) => {
+  //     logger.error('mongodb error', e);
+  //     process.exit(1);
+  //   });
 
-  return connection.connection.db;
+  // return connection.connection.db;
+  return '';
 };
