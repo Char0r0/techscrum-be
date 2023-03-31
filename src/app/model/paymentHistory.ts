5b515d88584c5aea6af347fd4bb2edd3e11ab100
Andy Wei <z5183885@ad.unsw.edu.au>
2023-03-31 23:33:35 +1100

    fix some small problems

--- src/app/model/paymentHistory.ts
+++ src/app/model/paymentHistory.ts
@@ -11,7 +11,6 @@ const paymentHistorySchema = new mongoose.Schema(
       type: String,
     },
 
-    // not necessary
     subscriptionId: {
       type: String,
     },
@@ -28,7 +27,6 @@ const paymentHistorySchema = new mongoose.Schema(
       type: String,
     },
     
-    // not necessary
     amount: {
       type: Number,
     },
