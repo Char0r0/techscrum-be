0aece9963cd515e2d3d3c883f10321d0656bf12c
kitman20022002 <kitman200220022002@gmail.com>
2023-04-01 10:09:41 +1100

    update db password

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -1,21 +1,19 @@
+/* eslint-disable no-secrets/no-secrets */
 const dotenv = require('dotenv');
 const stripeAPI = require('stripe');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 dotenv.config();
 
-const DEFAULT_MONGODB_URL =
-  'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/techscrumapp?retryWrites=true&w=majority';
-const DEFAULT_TANANT_CONNECTION =
-  'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/tenant?retryWrites=true&w=majority';
-const DEFAULT_DOMAIN_CONNECTION =
-  'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/domain?retryWrites=true&w=majority';
+const DEFAULT_MONGODB_URL = `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.c7jps.mongodb.net/techscrumapp?retryWrites=true&w=majority`;
+const DEFAULT_TANANT_CONNECTION = `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.c7jps.mongodb.net/tenant?retryWrites=true&w=majority`;
+const DEFAULT_DOMAIN_CONNECTION = `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.c7jps.mongodb.net/domain?retryWrites=true&w=majority`;
 //---------------------------v2--------------------------
 const DEFAULT_USER_CONNECTION =
   // eslint-disable-next-line no-secrets/no-secrets
-  'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/users?retryWrites=true&w=majority';
+  `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.c7jps.mongodb.net/users?retryWrites=true&w=majority`;
 const DEFAULT_PUBLIC_CONNECTION =
   // eslint-disable-next-line no-secrets/no-secrets
-  'mongodb+srv://admin:12345678910@cluster0.c7jps.mongodb.net/publicdb?retryWrites=true&w=majority';
+  `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.c7jps.mongodb.net/publicdb?retryWrites=true&w=majority`;
 
 module.exports = {
   name: process.env.NAME || 'techscrumapp',
@@ -39,4 +37,3 @@ module.exports = {
   publicConnection: process.env.PUBLIC_URL || DEFAULT_PUBLIC_CONNECTION,
   authenticationConnection: process.env.DB_V1 || DEFAULT_USER_CONNECTION,
 };
-
