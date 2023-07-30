a39d6efe6e3fc7fbef16a8a6a1e334af1b9f886e
kitman <kitman200220022002@gmail.com>
2023-07-30 11:36:01 +1000

    added important console message

--- src/app/console/init.ts
+++ src/app/console/init.ts
@@ -63,7 +63,7 @@ const init = async (domainInput:string, emailInput:string, passwordInput:string)
 console.log('\x1b[31mDEVOPS IMPORTANT!!! DON"T use the default email and password for PRODUCTION environment, SERIOUS SECURITY ISSUE!!!\x1b[0m');
 rl.question('Please type confirm that you have READ THIS MESSAGE: ',  (answer:string) => {
   if (answer.toLowerCase() === 'confirm') {
-    rl.question('Please enter the frontend domain (http://localhost:3000): ', (domain:string) => {
+    rl.question('Please enter the FRONTEND domain (http://localhost:3000): ', (domain:string) => {
       rl.question('Please enter the user email (techscrum@gmail.com): ', (email:string) => {
         rl.question('Please enter the user password (12345678): ', (password:string) => {
           init(domain, email, password);
