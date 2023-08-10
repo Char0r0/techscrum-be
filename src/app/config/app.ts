fbe2b1b38b06a57c7e57fa55b607f047d1c60ab8
kitman <kitman200220022002@gmail.com>
2023-08-10 22:42:32 +1000

    remove uused var

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -4,16 +4,10 @@ const stripeAPI = require('stripe');
 process.env.NODE_ENV = process.env.NODE_ENV ?? 'development';
 dotenv.config();
 
-const DEFAULT_MONGODB_URL = `mongodb+srv://dbuser:${process.env.DB_PASSWORD}@cluster0.c7jps.mongodb.net/techscrumapp?retryWrites=true&w=majority`;
-const DEFAULT_TANANT_CONNECTION = `mongodb+srv://dbuser:${process.env.DB_PASSWORD}@cluster0.c7jps.mongodb.net/tenant?retryWrites=true&w=majority`;
-const DEFAULT_DOMAIN_CONNECTION = `mongodb+srv://dbuser:${process.env.DB_PASSWORD}@cluster0.c7jps.mongodb.net/domain?retryWrites=true&w=majority`;
 //---------------------------v2--------------------------
 const DEFAULT_USER_CONNECTION =
   // eslint-disable-next-line no-secrets/no-secrets
   `mongodb+srv://dbuser:${process.env.DB_PASSWORD}@cluster0.c7jps.mongodb.net/users?retryWrites=true&w=majority`;
-const DEFAULT_PUBLIC_CONNECTION =
-  // eslint-disable-next-line no-secrets/no-secrets
-  `mongodb+srv://dbuser:${process.env.DB_PASSWORD}@cluster0.c7jps.mongodb.net/publicdb?retryWrites=true&w=majority`;
 
 export const config = {
   name: process.env.NAME ?? 'techscrumapp',
@@ -22,20 +16,19 @@ export const config = {
     prefix: process.env.API_PREFIX ?? '/api/v1',
   },
   version: '1.0.0',
-  db: process.env.MONGODB_URL ?? DEFAULT_MONGODB_URL,
-  tenantConnection: process.env.TENANT_MONGODB_URL ?? DEFAULT_TANANT_CONNECTION,
-  domainConnection: process.env.DOMAIN_MONGODB_URL ?? DEFAULT_DOMAIN_CONNECTION,
-  useDefaultDatabase: process.env.USE_DEFAULT_DATABASE ?? true,
-  companyAddress: process.env.COMPANY_ADDRESS ?? 'su93031093@gmail.com',
-  defaultTenantConnection: process.env.DEFAULT_TENANT_CONNECTION ?? 'devtechscrumapp',
+  db: process.env.MONGODB_URL ?? null, //remove later on
+  tenantConnection: process.env.TENANT_MONGODB_URL ?? null, //remove later on
+  useDefaultDatabase: process.env.USE_DEFAULT_DATABASE ?? true, //remove later on
+  companyAddress: process.env.COMPANY_ADDRESS ?? null,
+  defaultTenantConnection: process.env.DEFAULT_TENANT_CONNECTION ?? null, //remove later on
   emailSecret: process.env.EMAIL_SECRET ?? '123456',
   forgotSecret: process.env.FORGET_SECRET ?? '321654',
   stripe: stripeAPI(process.env.STRIPE_PRIVATE_KEY),
   stripeSecret: process.env.STRIPE_WEBHOOK_SECRET,
   //---------------------------v2--------------------------
-  tenantsDBConnection: process.env.TENANTS_CONNECTION ?? DEFAULT_USER_CONNECTION,
-  publicConnection: process.env.PUBLIC_CONNECTION ?? DEFAULT_PUBLIC_CONNECTION,
-  authenticationConnection: process.env.DB_V1 ?? DEFAULT_USER_CONNECTION,
+  tenantsDBConnection: process.env.TENANTS_CONNECTION ?? null,
+  publicConnection: process.env.PUBLIC_CONNECTION ?? null,
+  authenticationConnection: process.env.DB_V1 ?? DEFAULT_USER_CONNECTION, //remove later on
   environment: process.env.ENVIRONMENT ?? 'production',
   connectTenantsOrigin: process.env.CONNECT_TENANT ?? null,
   mainDomain: process.env.MAIN_DOMAIN,
